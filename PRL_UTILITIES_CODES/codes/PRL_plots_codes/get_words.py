b2115f4da1d9367ce1286ba2aef2f96832d7bcb4
Carlos Yero <nuclear@deuteron.local>
2020-10-03 13:21:30 -0400

    finalized supp. materials and modifications of prl text / images

--- PRL_UTILITIES_CODES/codes/PRL_plots_codes/get_words.py
+++ PRL_UTILITIES_CODES/codes/PRL_plots_codes/get_words.py
@@ -11,10 +11,12 @@ def get_words(w=0, h=0, ncol=0):
     if(ncol==1):
         #For single column figures, the word estimate for a figure size is:
         one_column_fig_words = (150./aspect_ratio) + 20.  #units: number of words
+        print('aspect ratio = ', aspect_ratio)
         print('(single column) # of words equivalent for figsize: (w=',w,' h=', h,') in. -----> ',one_column_fig_words,' words')
     elif(ncol==2):
         #For figures that occupy both columns of the paper, the word estimate for a figsize is:
         two_column_fig_words = (300./(0.5*aspect_ratio) ) + 40. 
+        print('aspect ratio = ', aspect_ratio)
         print('(two columns) # of words equivalent for figsize: (w=',w,' h=', h,') in. -----> ',two_column_fig_words,' words')
 
 
